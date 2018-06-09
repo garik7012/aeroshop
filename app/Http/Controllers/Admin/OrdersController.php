4baf42984a6688a6ebe8bdfa121e843d9accd6a7
Igor <igor.grin.85@gmail.com>
2018-06-09 23:53:09 +0300

    order delete

--- app/Http/Controllers/Admin/OrdersController.php
+++ app/Http/Controllers/Admin/OrdersController.php
@@ -9,11 +9,22 @@ use App\Http\Controllers\Controller;
 
 class OrdersController extends Controller
 {
+    /**
+     * show order list
+     * @param Order $order
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index(Order $order)
     {
         return view('admin.order.all', ['orders' => $order->all()]);
     }
 
+    /**
+     * order details
+     * @param $id
+     * @param Order $orders
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function showOrder($id, Order $orders)
     {
         $order = $orders->findOrFail($id);
@@ -25,6 +36,12 @@ class OrdersController extends Controller
         return view('admin.order.show', compact('order', 'total'));
     }
 
+    /**
+     * set is complete
+     * @param $id
+     * @param Order $orders
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function updateOrder($id, Order $orders)
     {
         $order = $orders->findOrFail($id);
@@ -32,5 +49,19 @@ class OrdersController extends Controller
         $order->save();
 
         return back()->with('success', 'Успешно обновлено');
+    }
+
+    /**
+     * delete order
+     * @param $id
+     * @param Order $order
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function deleteOrder($id, Order $order)
+    {
+        $order = $order->findOrFail($id);
+        $order->delete();
+
+        return back()->with('success', 'Заказ успешно удален');
     }
 }
